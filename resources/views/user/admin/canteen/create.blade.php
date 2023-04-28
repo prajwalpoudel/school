886a73f7923ca7a5433ef89d687d6688f9e22e8c
Madan Neupane <nmadan692@gmail.com>
2023-04-28 15:56:29 +0900

    optimized code for canteen section

--- resources/views/user/admin/canteen/create.blade.php
+++ resources/views/user/admin/canteen/create.blade.php
@@ -1,19 +1,15 @@
 @extends('user.admin.layouts.master')
-@section('breadcrumbs')
-    {!! $breadcrumbs !!}
-@endsection
+
+@section('breadcrumbs', $breadcrumbs)
+
 @section('content')
     <div class="m-content">
-        <div class="m-content">
-            @include('user.admin.canteen.partials.form',
-                        $data=[
-                            'form-action' => 'admin.canteen.store',
-                            'form-method' => 'post',
-                            'form-title' => 'Create Canteen',
-                            'button-text' => 'Save',
-                            'canteen' => null
-                        ]
-            )
-        </div>
+        @include('user.admin.canteen.partials.form', [
+            'form-action' => 'admin.canteen.store',
+            'form-method' => 'post',
+            'form-title' => 'Create Canteen',
+            'button-text' => 'Save',
+            'canteen' => null,
+        ])
     </div>
 @endsection
